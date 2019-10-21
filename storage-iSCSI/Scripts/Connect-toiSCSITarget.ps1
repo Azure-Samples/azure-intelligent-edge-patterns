b106a60fd9731e16b5c66e0c548652dff6eb709f
lucidqdreams <matthew.quickenden@gmail.com>
2019-10-21 11:36:05 -0700

    connect

--- storage-iSCSI/Scripts/Connect-toiSCSITarget.ps1
+++ storage-iSCSI/Scripts/Connect-toiSCSITarget.ps1
@@ -65,25 +65,3 @@ Foreach ($TargetPortalAddress in $TargetPortalAddresses)
     }
 }
 #>
-
-Foreach ($TargetPortalAddress in $TargetPortalAddresses){
-    New-IscsiTargetPortal -TargetPortalAddress $TargetPortalAddress -TargetPortalPortNumber 3260 -InitiatorPortalAddress $LocaliSCSIAddress
-}
-
- 
-Foreach ($TargetPortalAddress in $TargetPortalAddresses){
-    Get-IscsiTarget | Connect-IscsiTarget -IsMultipathEnabled $true -TargetPortalAddress $TargetPortalAddress -InitiatorPortalAddress $LocaliSCSIAddress -IsPersistent $true
-}
-
-switch  ($LoadBalancePolicy) 
-{
-    "FOO" {write-verbose "Load Balance Policy Set to 'FOO' Fail Over Only."}
-    "RR" {write-verbose "Load Balance Policy Set to 'RR' Round Robin."}
-    "LQD" {write-verbose "Load Balance Policy Set to 'LQD' Least Queue Depth."}
-    "LB" {write-verbose "Load Balance Policy Set to 'LB' Least Blocks."}
-}
-
-
-Set-MSDSMGlobalDefaultLoadBalancePolicy -Policy $LoadBalancePolicy
-
- Get-IscsiConnection
