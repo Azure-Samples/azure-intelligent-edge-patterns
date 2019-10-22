6e7d16fb60147d07b886bdd5d2b987af0bf59a72
lucidqdreams <matthew.quickenden@gmail.com>
2019-10-22 13:52:28 -0700

    process

--- storage-iSCSI/Scripts/Prepare-iSCSITarget.ps1
+++ storage-iSCSI/Scripts/Prepare-iSCSITarget.ps1
@@ -6,7 +6,7 @@ param
         $RemoteServer = "FileServer",
         [Parameter(ParameterSetName='iSCSIDisk')]
         [Array]
-        $RemoteServerIPs = @("1.1.1.1","1.1.1.2"),
+        $RemoteServerIPs = @("1.1.1.1"),
         [Parameter(ParameterSetName='iSCSIDisk')]
         [String]
         $DiskFolder = 'C:\iscsishare',    
