5ed1ce11161765167a21914165ed2c4b0416895c
Andrew Kao <andykao1213@gmail.com>
2020-03-27 15:45:47 +0800

    Add useCameras
    Extract the proccess of getting camera in Camera.tsx to useCameras

--- VisionOnEdge/ui/src/State.ts
+++ VisionOnEdge/ui/src/State.ts
@@ -1,5 +1,7 @@
-export interface State { cameras: Camera[] };
-export type Camera = { id?: number, name: string, rtsp: string, model_name: string }
+export interface State {
+  cameras: Camera[];
+}
+export type Camera = { id?: number; name: string; rtsp: string; model_name: string };
 export const initialState: State = {
   cameras: [],
-}
+};
