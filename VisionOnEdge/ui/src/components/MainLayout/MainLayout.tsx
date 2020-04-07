4f138cf4f27b01e7f11e8ae630fe16e3d0b21e45
Andrew Kao <andrewkao@linkernetworks.com>
2020-04-07 16:03:10 +0800

    Add Icon and ReDesign Left Nav

--- VisionOnEdge/ui/src/components/MainLayout/MainLayout.tsx
+++ VisionOnEdge/ui/src/components/MainLayout/MainLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Grid, Segment, Menu } from '@fluentui/react-northstar';
-import { Link } from 'react-router-dom';
+import { Grid, Segment, Image, Flex } from '@fluentui/react-northstar';
+import { NavLink } from 'react-router-dom';
 
 export const MainLayout: React.FC = ({ children }) => {
   return (
@@ -23,26 +23,26 @@ export const MainLayout: React.FC = ({ children }) => {
 };
 
 const Nav = ({ styles }): JSX.Element => {
-  const items = [
-    {
-      key: 'cameras',
-      as: Link,
-      to: '/cameras',
-      icon: 'call-video',
-    },
-    {
-      key: 'parts',
-      as: Link,
-      to: '/parts',
-      icon: 'settings',
-    },
-    {
-      key: 'none',
-      as: Link,
-      to: '/',
-      icon: 'user-friends',
-    },
-  ];
+  return (
+    <Segment color="grey" inverted styles={{ ...styles, padding: 0, paddingTop: '1em' }}>
+      <Flex column gap="gap.large" hAlign="center">
+        <NavItem src="/icons/location.png" to="/location"></NavItem>
+        <NavItem src="/icons/camera.png" to="/cameras"></NavItem>
+        <NavItem src="/icons/part.png" to="/parts"></NavItem>
+        <NavItem src="/icons/manual.png" to="/manual"></NavItem>
+      </Flex>
+    </Segment>
+  );
+};
 
-  return <Menu items={items} styles={styles} vertical pointing />;
+const NavItem = ({ src, to }): JSX.Element => {
+  return (
+    <NavLink
+      to={to}
+      style={{ display: 'flex', justifyContent: 'center', padding: '0.8em' }}
+      activeStyle={{ backgroundColor: 'rgba(250, 83, 5, 0.5)' }}
+    >
+      <Image src={src} design={{ width: '100%' }}></Image>
+    </NavLink>
+  );
 };
