3fe947f28e0edc5835fcddd09b5ad24a7603c665
Andrew Kao <andrewkao@linkernetworks.com>
2020-04-16 18:06:21 +0800

    Finish Image Upload 1. Add fields in ImageSerializer 2. Reset Capture Image After Upload

--- VisionOnEdge/ui/src/components/UploadPhotos/UploadPhotos.tsx
+++ VisionOnEdge/ui/src/components/UploadPhotos/UploadPhotos.tsx
@@ -4,47 +4,10 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { State } from '../../store/State';
 import { Part } from '../../store/part/partTypes';
-import { thunkGetCapturedImages } from '../../store/part/partActions';
+import { thunkGetCapturedImages, addCapturedImages } from '../../store/part/partActions';
 import LabelingPageDialog from '../LabelingPageDialog';
 
 export const UploadPhotos = ({ partId }): JSX.Element => {
-  return (
-    <>
-      <input
-        type="file"
-        onChange={(e): void => {
-          handleUpload(e, partId);
-        }}
-        accept="image/*"
-        multiple
-      />
-      <CapturedImagesContainer />
-    </>
-  );
-};
-
-async function handleUpload(e: React.ChangeEvent<HTMLInputElement>, partId: number): Promise<void> {
-  const requests: Promise<Response>[] = [];
-  for (let i = 0; i < e.target.files.length; i++) {
-    const formData = new FormData();
-    formData.append('image', e.target.files[i]);
-    requests.push(
-      fetch(`/api/images/?part_id=${partId}/`, {
-        method: 'POST',
-        body: formData,
-      }),
-    );
-  }
-
-  try {
-    const result = await Promise.all(requests);
-    console.log(result);
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-const CapturedImagesContainer = (): JSX.Element => {
   const dispatch = useDispatch();
   const { capturedImages } = useSelector<State, Part>((state) => state.part);
 
@@ -52,6 +15,30 @@ const CapturedImagesContainer = (): JSX.Element => {
     dispatch(thunkGetCapturedImages());
   }, [dispatch]);
 
+  function handleUpload(e: React.ChangeEvent<HTMLInputElement>): void {
+    for (let i = 0; i < e.target.files.length; i++) {
+      const formData = new FormData();
+      formData.append('image', e.target.files[i]);
+      formData.append('part', `http://localhost:8000/api/parts/${partId}/`);
+      fetch(`/api/images/`, {
+        method: 'POST',
+        body: formData,
+      })
+        .then((res) => res.json())
+        .then((data) => dispatch(addCapturedImages(data)))
+        .catch((err) => console.error(err));
+    }
+  }
+
+  return (
+    <>
+      <input type="file" onChange={handleUpload} accept="image/*" multiple />
+      <CapturedImagesContainer capturedImages={capturedImages} />
+    </>
+  );
+};
+
+const CapturedImagesContainer = ({ capturedImages }): JSX.Element => {
   return (
     <div style={{ border: '1px solid grey', height: '100%', padding: '10px' }}>
       <Flex gap="gap.medium" wrap>
