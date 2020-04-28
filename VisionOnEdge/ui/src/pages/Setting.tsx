e61cf5139849ced1895c65b35a18fc6ad7b7c314
Andrew Kao <andrewkao@linkernetworks.com>
2020-04-28 17:48:49 +0800

    Add new fields in setting page

--- VisionOnEdge/ui/src/pages/Setting.tsx
+++ VisionOnEdge/ui/src/pages/Setting.tsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Divider, Flex, Text, Input, Button, Alert } from '@fluentui/react-northstar';
 import { Link } from 'react-router-dom';
+import Axios from 'axios';
 
 export const Setting = (): JSX.Element => {
   const [settingData, setSettingData] = useState({
     id: null,
     key: '',
     namespace: '',
+    iotHubConnectionString: '',
+    deviceId: '',
+    moduleId: '',
   });
   const [saveStatus, setSaveStatus] = useState({
     success: false,
@@ -14,14 +18,16 @@ export const Setting = (): JSX.Element => {
   });
 
   useEffect(() => {
-    fetch('/api/settings/')
-      .then((res) => res.json())
-      .then((data) => {
+    Axios.get('/api/settings/')
+      .then(({ data }) => {
         if (data.length > 0) {
           setSettingData({
             id: data[0].id,
             key: data[0].training_key,
             namespace: data[0].endpoint,
+            iotHubConnectionString: data[0].iot_hub_connection_string,
+            deviceId: data[0].device_id,
+            moduleId: data[0].module_id,
           });
         }
         return void 0;
@@ -35,17 +41,16 @@ export const Setting = (): JSX.Element => {
     const isSettingEmpty = settingData.id === null;
     const url = isSettingEmpty ? `/api/settings/` : `/api/settings/${settingData.id}/`;
 
-    fetch(url, {
-      body: JSON.stringify({
+    Axios(url, {
+      data: {
         training_key: settingData.key,
         endpoint: settingData.namespace,
-      }),
-      method: isSettingEmpty ? 'POST' : 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
+        iot_hub_connection_string: settingData.iotHubConnectionString,
+        device_id: settingData.deviceId,
+        module_id: settingData.moduleId,
       },
+      method: isSettingEmpty ? 'POST' : 'PUT',
     })
-      .then((res) => res.json())
       .then(() => {
         setSaveStatus({ success: true, content: 'Save Setting Successfully' });
         return void 0;
@@ -80,8 +85,37 @@ export const Setting = (): JSX.Element => {
           onChange={(_, { value }): void => setSettingData((prev) => ({ ...prev, key: value }))}
         />
       </Flex>
+      <Flex vAlign="center">
+        <Text size="large" design={{ width: '300px' }}>
+          Iot Hub Connection String:
+        </Text>
+        <Input
+          value={settingData.iotHubConnectionString}
+          onChange={(_, { value }): void =>
+            setSettingData((prev) => ({ ...prev, iotHubConnectionString: value }))
+          }
+        />
+      </Flex>
+      <Flex vAlign="center">
+        <Text size="large" design={{ width: '300px' }}>
+          Device ID:
+        </Text>
+        <Input
+          value={settingData.deviceId}
+          onChange={(_, { value }): void => setSettingData((prev) => ({ ...prev, deviceId: value }))}
+        />
+      </Flex>
+      <Flex vAlign="center">
+        <Text size="large" design={{ width: '300px' }}>
+          Module ID:
+        </Text>
+        <Input
+          value={settingData.moduleId}
+          onChange={(_, { value }): void => setSettingData((prev) => ({ ...prev, moduleId: value }))}
+        />
+      </Flex>
       <Flex gap="gap.large">
-        <Button primary onClick={onSave}>
+        <Button primary onClick={onSave} disabled={Object.values(settingData).some((e) => !e)}>
           Save
         </Button>
         <Button primary as={Link} to="/">
