4023c5cfdf2bc848e49d911b8fa58a79f10ed5fa
Andrew Kao <andrewkao@linkernetworks.com>
2020-05-12 18:24:16 +0800

    Remove LabelingPage from RootRouter

--- VisionOnEdge/EdgeSolution/modules/WebModule/ui/src/routes/RootRouter.tsx
+++ VisionOnEdge/EdgeSolution/modules/WebModule/ui/src/routes/RootRouter.tsx
@@ -4,7 +4,6 @@ import Home from '../pages/Home';
 import Cameras from '../pages/Cameras';
 import CameraDetails from '../pages/CameraDetails';
 import { PartDetails } from '../pages/PartDetails';
-import LabelingPage from '../pages/LabelingPage';
 import Locations from '../pages/Locations';
 import LocationRegister from '../pages/LocationRegister';
 import LocationDetails from '../pages/LocationDetails';
@@ -20,7 +19,6 @@ export const RootRouter: FC = () => {
       <Route path="/locations/register" component={LocationRegister} />
       <Route path="/locations/detail" component={LocationDetails} />
       <Route path="/locations" component={Locations} />
-      <Route path="/label/:imageIndex" component={LabelingPage} />
       <Route path="/cameras/detail" component={CameraDetails} />
       <Route path="/cameras" component={Cameras} />
       <Route path="/parts/detail" component={PartDetails} />
