c3e9986f15d217db3575a20d4f394dc7a8943a84
Willy Kuo <waitingkuo0527@gmail.com>
2020-05-13 03:58:13 +0800

    update ui production

--- VisionOnEdge/EdgeSolution/modules/WebModule/backend/ui_production/precache-manifest.cd78b8ade957bfdac654e956178a5fc2.js
+++ VisionOnEdge/EdgeSolution/modules/WebModule/backend/ui_production/precache-manifest.cd78b8ade957bfdac654e956178a5fc2.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1494af1622c28d95b8dba65a3e4191b2",
+    "revision": "02615567c091732ea5a00b372b430d37",
     "url": "/index.html"
   },
   {
-    "revision": "d9c896078a730ceb821a",
+    "revision": "32a2b659ced7c72672e9",
     "url": "/static/css/2.37a6b064.chunk.css"
   },
   {
-    "revision": "9341a776eccd1499addd",
+    "revision": "c16f32fc81b7589cd1bc",
     "url": "/static/css/main.b222b7b7.chunk.css"
   },
   {
-    "revision": "d9c896078a730ceb821a",
-    "url": "/static/js/2.d4891ecb.chunk.js"
+    "revision": "32a2b659ced7c72672e9",
+    "url": "/static/js/2.2d621641.chunk.js"
   },
   {
     "revision": "5f6590bd797589ba0b36aa042b245e0e",
-    "url": "/static/js/2.d4891ecb.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.2d621641.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9341a776eccd1499addd",
-    "url": "/static/js/main.ced18e8b.chunk.js"
+    "revision": "c16f32fc81b7589cd1bc",
+    "url": "/static/js/main.4b622686.chunk.js"
   },
   {
     "revision": "f18abcc333149a0eb80f",
