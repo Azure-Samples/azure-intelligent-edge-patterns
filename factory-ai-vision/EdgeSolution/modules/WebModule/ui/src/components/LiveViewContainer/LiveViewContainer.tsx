f1649cf2be65bcefe45a68950edda6b0cb4d9e83
Andrew Kao <andrewkao@linkernetworks.com>
2020-05-26 16:02:29 +0800

    Add Event Callback for Button

--- factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/components/LiveViewContainer/LiveViewContainer.tsx
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/components/LiveViewContainer/LiveViewContainer.tsx
@@ -1,25 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { Text, Image } from '@fluentui/react-northstar';
-
-enum UpdateStatus {
-  Idle,
-  Ready,
-  Loading,
-  Success,
-  Failed,
-}
+import { Text, Image, Checkbox, Flex, Button } from '@fluentui/react-northstar';
 
 export const LiveViewContainer: React.FC<{ showVideo: boolean }> = ({ showVideo }) => {
   const [showAOI, setShowAOI] = useState(true);
   const [AOIs, setAOIs] = useState<{ x1: number; y1: number; x2: number; y2: number }[]>([]);
-  const [updateStatus, setUpdateStatus] = useState(UpdateStatus.Idle);
+  const [showUpdateSuccessTxt, setShowUpdateSuccessTxt] = useState(false);
+  const [hasEdit, setHasEdit] = useState(true);
+
+  const onCheckboxClick = () => {
+    setShowAOI((prev) => !prev);
+  };
+
+  const onUpdate = () => {
+    // TODO API
+    setShowUpdateSuccessTxt(true);
+  };
+
+  useEffect(() => {
+    if (showUpdateSuccessTxt) {
+      const timer = setTimeout(() => {
+        setShowUpdateSuccessTxt(false);
+      }, 3000);
+      return (): void => clearTimeout(timer);
+    }
+  }, [showUpdateSuccessTxt]);
+
+  const updateBtnDisabled = !showAOI || !hasEdit;
 
   return (
-    <>
+    <Flex column gap="gap.medium">
       <Text styles={{ width: '150px' }} size="large">
         Live View:
       </Text>
+      <Flex hAlign="end" gap="gap.small" vAlign="center">
+        <Checkbox
+          labelPosition="start"
+          label="Show area of interest"
+          toggle
+          checked={showAOI}
+          onClick={onCheckboxClick}
+        />
+        <Button content="Update" primary disabled={updateBtnDisabled} onClick={onUpdate} />
+        <Text styles={{ visibility: showUpdateSuccessTxt ? 'visible' : 'hidden' }}>Updated!</Text>
+      </Flex>
       <div style={{ width: '100%', height: '600px', backgroundColor: 'black' }}>
         {showVideo ? (
           <Image
@@ -28,6 +52,6 @@ export const LiveViewContainer: React.FC<{ showVideo: boolean }> = ({ showVideo 
           />
         ) : null}
       </div>
-    </>
+    </Flex>
   );
 };
