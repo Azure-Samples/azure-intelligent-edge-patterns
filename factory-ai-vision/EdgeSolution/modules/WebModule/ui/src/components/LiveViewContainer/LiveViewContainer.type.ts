3db0df39c23db18eb07aa01eeb81f72486cf3559
Andrew Kao <andrewkao@linkernetworks.com>
2020-05-27 17:35:18 +0800

    Handle Visibility

--- factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/components/LiveViewContainer/LiveViewContainer.type.ts
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/components/LiveViewContainer/LiveViewContainer.type.ts
@@ -5,15 +5,18 @@ export type Box = { x1: number; y1: number; x2: number; y2: number };
 export type LiveViewProps = {
   AOIs: Box[];
   setAOIs: Dispatch<SetStateAction<Box[]>>;
+  visible: boolean;
 };
 
 export type MaskProps = {
   width: number;
   height: number;
   holes: Box[];
+  visible: boolean;
 };
 
 export type AOIBoxProps = {
   box: Box;
   onBoxChange: (updateBox: (prevBox: Box) => Box) => void;
+  visible: boolean;
 };
