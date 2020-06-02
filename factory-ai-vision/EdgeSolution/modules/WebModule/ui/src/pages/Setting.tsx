4e8269fda95677469400b48b54eacaae05806c17
Andrew Kao <andrewkao@linkernetworks.com>
2020-06-02 15:27:47 +0800

    New Setting Layout

--- factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/pages/Setting.tsx
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/pages/Setting.tsx
@@ -1,33 +1,81 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer, Reducer } from 'react';
+import * as R from 'ramda';
 import { Divider, Flex, Text, Input, Button, Alert, Dropdown } from '@fluentui/react-northstar';
 import { Link } from 'react-router-dom';
 import Axios from 'axios';
 
-export const Setting = (): JSX.Element => {
-  const [settingData, setSettingData] = useState({
-    id: null,
+const initialState = {
+  current: {
+    id: -1,
     key: '',
     namespace: '',
-    iotHubConnectionString: '',
-    deviceId: '',
-    moduleId: '',
-  });
+  },
+  origin: {
+    id: -1,
+    key: '',
+    namespace: '',
+  },
+};
+
+type SettingDataState = typeof initialState;
+
+type Action =
+  | {
+      type: 'UPDATE_KEY';
+      payload: string;
+    }
+  | {
+      type: 'UPDATE_NAMESPACE';
+      payload: string;
+    }
+  | {
+      type: 'UPDATE_ALL';
+      payload: SettingDataState;
+    };
+
+type SettingReducer = Reducer<SettingDataState, Action>;
+
+const reducer: SettingReducer = (state, action) => {
+  switch (action.type) {
+    case 'UPDATE_KEY':
+      return { ...state, current: { ...state.current, key: action.payload } };
+    case 'UPDATE_NAMESPACE':
+      return { ...state, current: { ...state.current, namespace: action.payload } };
+    case 'UPDATE_ALL':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export const Setting = (): JSX.Element => {
   const [saveStatus, setSaveStatus] = useState({
     success: false,
     content: '',
   });
+  const [{ current: settingData, origin: originSettingData }, dispatch] = useReducer<SettingReducer>(
+    reducer,
+    initialState,
+  );
 
   useEffect(() => {
     Axios.get('/api/settings/')
       .then(({ data }) => {
         if (data.length > 0) {
-          setSettingData({
-            id: data[0].id,
-            key: data[0].training_key,
-            namespace: data[0].endpoint,
-            iotHubConnectionString: data[0].iot_hub_connection_string,
-            deviceId: data[0].device_id,
-            moduleId: data[0].module_id,
+          dispatch({
+            type: 'UPDATE_ALL',
+            payload: {
+              current: {
+                id: data[0].id,
+                key: data[0].training_key,
+                namespace: data[0].endpoint,
+              },
+              origin: {
+                id: data[0].id,
+                key: data[0].training_key,
+                namespace: data[0].endpoint,
+              },
+            },
           });
         }
         return void 0;
@@ -38,27 +86,43 @@ export const Setting = (): JSX.Element => {
   }, []);
 
   const onSave = (): void => {
-    const isSettingEmpty = settingData.id === null;
+    const isSettingEmpty = settingData.id === -1;
     const url = isSettingEmpty ? `/api/settings/` : `/api/settings/${settingData.id}/`;
 
     Axios(url, {
       data: {
         training_key: settingData.key,
         endpoint: settingData.namespace,
-        iot_hub_connection_string: settingData.iotHubConnectionString,
-        device_id: settingData.deviceId,
-        module_id: settingData.moduleId,
       },
       method: isSettingEmpty ? 'POST' : 'PUT',
     })
-      .then(() => {
+      .then(({ data }) => {
         setSaveStatus({ success: true, content: 'Save Setting Successfully' });
+        dispatch({
+          type: 'UPDATE_ALL',
+          payload: {
+            current: {
+              id: data.id,
+              key: data.training_key,
+              namespace: data.endpoint,
+            },
+            origin: {
+              id: data.id,
+              key: data.training_key,
+              namespace: data.endpoint,
+            },
+          },
+        });
         return void 0;
       })
       .catch((err) => {
         setSaveStatus({ success: false, content: `Fail to save settings: /n ${err.toString()}` });
       });
   };
+
+  const notEmpty = settingData.id !== -1;
+
+  const cannotUpdateOrSave = R.equals(settingData, originSettingData);
 
   return (
     <>
@@ -75,7 +139,7 @@ export const Setting = (): JSX.Element => {
             </Text>
             <Input
               value={settingData.namespace}
-              onChange={(_, { value }): void => setSettingData((prev) => ({ ...prev, namespace: value }))}
+              onChange={(_, { value }): void => dispatch({ type: 'UPDATE_NAMESPACE', payload: value })}
               fluid
             />
           </Flex>
@@ -85,13 +149,13 @@ export const Setting = (): JSX.Element => {
             </Text>
             <Input
               value={settingData.key}
-              onChange={(_, { value }): void => setSettingData((prev) => ({ ...prev, key: value }))}
+              onChange={(_, { value }): void => dispatch({ type: 'UPDATE_KEY', payload: value })}
               fluid
             />
           </Flex>
           <Flex gap="gap.large">
-            <Button primary onClick={onSave} disabled={Object.values(settingData).some((e) => !e)}>
-              Save
+            <Button primary onClick={onSave} disabled={cannotUpdateOrSave}>
+              {notEmpty ? 'Update' : 'Save'}
             </Button>
             <Button primary as={Link} to="/">
               Cancel
@@ -106,14 +170,18 @@ export const Setting = (): JSX.Element => {
             />
           ) : null}
         </Flex>
-        <Divider color="grey" vertical styles={{ height: '100%' }} />
-        <Flex column gap="gap.large">
-          <Text size="large" weight="bold">
-            Previous Projects:{' '}
-          </Text>
-          <Dropdown />
-          <Button primary content="Load" />
-        </Flex>
+        {notEmpty && (
+          <>
+            <Divider color="grey" vertical styles={{ height: '100%' }} />
+            <Flex column gap="gap.large">
+              <Text size="large" weight="bold">
+                Previous Projects:{' '}
+              </Text>
+              <Dropdown />
+              <Button primary content="Load" />
+            </Flex>
+          </>
+        )}
       </Flex>
     </>
   );
