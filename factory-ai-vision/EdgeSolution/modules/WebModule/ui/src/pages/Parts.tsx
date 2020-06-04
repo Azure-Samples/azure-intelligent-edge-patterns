1257928a5b8dba32d4fe5cff4cf36e5d3911341f
Andrew Kao <andrewkao@linkernetworks.com>
2020-06-04 12:15:44 +0800

    Use Filter instead of query non demo part

--- factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/pages/Parts.tsx
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/pages/Parts.tsx
@@ -8,7 +8,7 @@ export const Parts: React.FC = () => {
   const [parts, setParts] = useState([]);
 
   useEffect(() => {
-    const partsAPI = Axios.get('/api/parts/?is_demo=0');
+    const partsAPI = Axios.get('/api/parts/');
     const imagesAPI = Axios.get('/api/images/');
 
     Axios.all([partsAPI, imagesAPI])
@@ -30,9 +30,11 @@ export const Parts: React.FC = () => {
   return (
     <div style={{ position: 'relative', height: '100%' }}>
       <Flex gap="gap.large" wrap>
-        {parts.map((ele) => (
-          <Item key={ele.id} src={ele.images} id={ele.id} name={ele.name} />
-        ))}
+        {parts
+          .filter((e) => !e.is_demo)
+          .map((ele) => (
+            <Item key={ele.id} src={ele.images} id={ele.id} name={ele.name} />
+          ))}
       </Flex>
       <Button
         primary
