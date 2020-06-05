6ab89a3a44223161a3cfc9c4e403ffd3c9304bba
Andrew Kao <andrewkao@linkernetworks.com>
2020-06-05 19:01:20 +0800

    Add Demo Condition in useProject

--- factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/hooks/useProject.ts
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/hooks/useProject.ts
@@ -8,13 +8,13 @@ import { thunkGetProject } from '../store/project/projectActions';
 /**
  * Fetch the project data from server and set to Redux Store
  */
-export const useProject = (): Project => {
+export const useProject = (isDemo?: boolean): Project => {
   const dispatch = useDispatch();
   const project = useSelector<State, Project>((state) => state.project);
 
   useEffect(() => {
-    dispatch(thunkGetProject());
-  }, [dispatch]);
+    dispatch(thunkGetProject(isDemo));
+  }, [dispatch, isDemo]);
 
   return project;
 };
