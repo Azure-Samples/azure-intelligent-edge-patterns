5d7068375a369e127defa5c569cd87ff583adeee
Aleksandr Panchul <apanchul@hotmail.com>
2020-06-09 23:46:27 -0700

    adding argument parsing to kubeflow install script

--- Research/kubeflow-on-azure-stack/sbin/kubeflow_install.sh
+++ Research/kubeflow-on-azure-stack/sbin/kubeflow_install.sh
@@ -5,17 +5,74 @@
 # It should be executed at the master node of your Kubernetes cluster
 #
 
+# default values
 export KF_CTL_DIR=~/kubeflow/
 export KF_INSTALL_LOG=$KF_CTL_DIR/install.log
-export PATH=$PATH:$KF_CTL_DIR
 export KF_NAME=sandboxASkf
 export KF_USERNAME=azureuser
 export KFCTL_RELEASE_FILENAME=kfctl_v1.0.2-0-ga476281_linux.tar.gz
 export KFCTL_RELEASE_URI="https://github.com/kubeflow/kfctl/releases/download/v1.0.2/${KFCTL_RELEASE_FILENAME}"
-export BASE_DIR=/opt
-export KF_DIR=${BASE_DIR}/${KF_NAME}
+export KF_DIR_BASE=/opt
 export KF_CONFIG_FILENAME="kfctl_k8s_istio.v1.0.2.yaml"
 export KF_CONFIG_URI="https://raw.githubusercontent.com/kubeflow/manifests/v1.0-branch/kfdef/${KF_CONFIG_FILENAME}"
+
+while [[ $# > 0 ]]
+do
+  case "$1" in
+    --kf_ctl_dir)
+      KF_CTL_DIR="$2"
+      echo "set kf_ctl_dir to ${KF_CTL_DIR}"
+      shift
+      ;;
+    --kf_name)
+      KF_NAME="$2"
+      echo "set kf_name to ${KF_NAME}"
+      shift
+      ;;
+    --kf_username)
+      KF_USERNAME="$2"
+      echo "set kf_username to ${KF_USERNAME}"
+      shift
+      ;;
+    --kfctl_release_uri)
+      KFCTL_RELEASE_URI="$2"
+      echo "set kfctl_release_uri to ${KFCTL_RELEASE_URI}"
+      shift
+      ;;
+    --kf_dir_base)
+      KF_DIR_BASE="$2"
+      echo "set kf_dir_base to ${KF_DIR_BASE}"
+      shift
+      ;;
+    --kf_config_filename)
+      KF_CONFIG_FILENAME="$2"
+      echo "set kf_config_filename to ${KF_CONFIG_FILENAME}"
+      shift
+      ;;
+    --kf_config_uri)
+      KF_CONFIG_URI="$2"
+      echo "set kf_config_uri to ${KF_CONFIG_URI}"
+      shift
+      ;;
+    --help|*)
+      echo "Usage: kubeflow_install.sh [options]"
+      echo " "
+      echo "Options:"
+      echo "   --kf_ctl_dir        - where to download and install kfctl"
+      echo "   --kf_name           - name of the Kubeflow cluster"
+      echo "   --kf_username       - user name under which to install Kubeflow"
+      echo "   --kfctl_release_uri - kfctl URI"
+      echo "   --kf_dir_base       - the base dir where you can install multiple instances of Kubeflow"
+      echo "   --kf_config_uri     - config URI of Kubeflow config"
+      echo "   --help"
+      exit 1
+      ;;        
+  esac
+  shift
+done
+
+export PATH=$PATH:$KF_CTL_DIR
+export KF_DIR=${KF_DIR_BASE}/${KF_NAME}
 
 
 echo "Installing Kubeflow" | tee $KF_INSTALL_LOG
@@ -72,4 +129,3 @@ kfctl apply -V -f ${KF_CONFIG_URI}  || exit 2
 echo "The installation will take a while, and there will be some time needed to create the pods."
 echo "In a few minutes, check the resources deployed correctly in namespace 'kubeflow'"
 echo "kubectl get all -n kubeflow"
-
