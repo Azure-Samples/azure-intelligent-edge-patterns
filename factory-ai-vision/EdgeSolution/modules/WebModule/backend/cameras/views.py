25d46bf1533db574ca1c15db60f602ba98a31592
Willy Kuo <waitingkuo0527@gmail.com>
2020-06-12 00:31:11 +0800

    update

--- factory-ai-vision/EdgeSolution/modules/WebModule/backend/cameras/views.py
+++ factory-ai-vision/EdgeSolution/modules/WebModule/backend/cameras/views.py
@@ -824,6 +824,8 @@ def train(request, project_id):
         logger.info('Update parts f{parts}')
         requests.get('http://'+inference_module_url() +
                      '/update_parts', params={'parts': parts})
+        requests.get('http://'+inference_module_url()+'/update_retrain_parameters', params={
+                'confidence_min': 30, 'confidence_max': 30, 'max_images': 10})
 
         obj, created = project_obj.upcreate_training_status(
             status='ok',
@@ -840,12 +842,18 @@ def train(request, project_id):
     logger.info('sleeping')
 
     rtsp = project_obj.camera.rtsp
+    parts = [p.name for p in project_obj.parts.all()]
+    download_uri = project_obj.download_uri
 
-    def _send(rtsp):
+    def _send(rtsp, parts, download_uri):
         logger.info(f'**** updaing cam to {rtsp}')
         requests.get('http://'+inference_module_url()+'/update_cam',
                      params={'cam_type': 'rtsp', 'cam_source': rtsp})
-    threading.Thread(target=_send, args=(rtsp,)).start()
+        requests.get('http://'+inference_module_url() +
+                     '/update_model', params={'model_uri': download_uri})
+        requests.get('http://'+inference_module_url() +
+                     '/update_parts', params={'parts': parts})
+    threading.Thread(target=_send, args=(rtsp, parts, download_uri)).start()
     return _train(project_id)
 
 
