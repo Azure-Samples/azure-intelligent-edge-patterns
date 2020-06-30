5f8fdf20cd624539aaebf1c97cb9073fda824aeb
Andrew Kao <andrewkao@linkernetworks.com>
2020-06-30 13:48:10 +0800

    Change Part Detail UI

--- factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/pages/PartDetails.tsx
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/pages/PartDetails.tsx
@@ -6,19 +6,21 @@ import axios from 'axios';
 import { CapturePhotos } from '../components/CapturePhoto';
 import { UploadPhotos } from '../components/UploadPhotos';
 import { useQuery } from '../hooks/useQuery';
+import { CapturedImagesContainer } from '../components/CapturePhoto/CapturePhotos';
 
 export const PartDetails = (): JSX.Element => {
   const partId = useQuery().get('partId');
+  const [goLabelImageIdx, setGoLabelImageIdx] = useState<number>(null);
 
   return (
     <Grid columns={'repeat(12, 1fr)'} styles={{ gridColumnGap: '20px', height: '100%' }}>
-      <LeftPanel partId={partId} />
-      <RightPanel partId={partId} />
+      <LeftPanel partId={partId} goLabelImageIdx={goLabelImageIdx} setGoLabelImageIdx={setGoLabelImageIdx} />
+      <RightPanel partId={partId} goLabelImageIdx={goLabelImageIdx} />
     </Grid>
   );
 };
 
-const LeftPanel = ({ partId }): JSX.Element => {
+const RightPanel = ({ partId, goLabelImageIdx }): JSX.Element => {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [error, setError] = useState('');
@@ -58,11 +60,19 @@ const LeftPanel = ({ partId }): JSX.Element => {
   };
 
   return (
-    <div style={{ height: '60%', display: 'flex', flexFlow: 'column', justifyContent: 'space-between', gridColumn: '1 / span 4' }}>
+    <div
+      style={{
+        height: '20%',
+        display: 'flex',
+        flexFlow: 'column',
+        justifyContent: 'space-between',
+        gridColumn: '8 / span 5',
+      }}
+    >
       <Input
         placeholder="Enter Part Name..."
         fluid
-        styles={{ fontSize: '2em' }}
+        // styles={{ fontSize: '1.5em' }}
         value={name}
         onChange={(_, newProps): void => {
           setName(newProps.value);
@@ -70,7 +80,7 @@ const LeftPanel = ({ partId }): JSX.Element => {
       />
       <TextArea
         placeholder="Enter Description..."
-        design={{ height: '60%' }}
+        // design={{ height: '60%' }}
         value={description}
         onChange={(_, newProps): void => {
           setDescription(newProps.value);
@@ -80,17 +90,22 @@ const LeftPanel = ({ partId }): JSX.Element => {
         <Button content="Save" primary onClick={onSave} disabled={!name} />
       </Flex>
       {!!error && <Alert danger content={error} dismissible />}
+      <CapturedImagesContainer partId={parseInt(partId, 10)} goLabelImageIdx={goLabelImageIdx} />
     </div>
   );
 };
 
-const RightPanel = ({ partId }): JSX.Element => {
+const LeftPanel = ({ partId, goLabelImageIdx, setGoLabelImageIdx }): JSX.Element => {
   return (
-    <Flex column gap="gap.small" styles={{ gridColumn: '5 / span 8' }}>
+    <Flex column gap="gap.small" styles={{ gridColumn: '1 / span 7' }}>
       {partId ? <Tab partId={partId} /> : null}
       <Switch>
         <Route path={`/parts/detail/capturePhotos`}>
-          <CapturePhotos partId={parseInt(partId, 10)} />
+          <CapturePhotos
+            partId={parseInt(partId, 10)}
+            goLabelImageIdx={goLabelImageIdx}
+            setGoLabelImageIdx={setGoLabelImageIdx}
+          />
         </Route>
         <Route path={`/parts/detail/uploadPhotos`}>
           <UploadPhotos partId={parseInt(partId, 10)} />
