2ca521946b4bea3ca85f6c8f1947b3dd9c935122
Willy Kuo <waitingkuo0527@gmail.com>
2020-07-01 19:50:22 +0800

    update ui

---
+++ factory-ai-vision/EdgeSolution/modules/WebModule/backend/ui_production/precache-manifest.207c8137af610ecb9e689ed9c9b4a9e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52bbb3e1d06baa519b9bde1233607be9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4da946d82f97e014b11d",
+    "url": "/static/css/2.37a6b064.chunk.css"
+  },
+  {
+    "revision": "93e57fc382c17660145a",
+    "url": "/static/css/main.b222b7b7.chunk.css"
+  },
+  {
+    "revision": "4da946d82f97e014b11d",
+    "url": "/static/js/2.b068418b.chunk.js"
+  },
+  {
+    "revision": "51fd16a6d4905803bde5c2d8538cda77",
+    "url": "/static/js/2.b068418b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93e57fc382c17660145a",
+    "url": "/static/js/main.5d8eb133.chunk.js"
+  },
+  {
+    "revision": "f18abcc333149a0eb80f",
+    "url": "/static/js/runtime-main.cc2604b0.js"
+  }
+]);
