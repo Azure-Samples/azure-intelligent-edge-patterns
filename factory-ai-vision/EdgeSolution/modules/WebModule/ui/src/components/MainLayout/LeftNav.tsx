1ae1c65e5f74d2bd972baf7b6563876ebad47b60
Andrew Kao <andrewkao@linkernetworks.com>
2020-07-15 16:55:49 +0800

    Add Toodtip on LeftNav Item

--- factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/components/MainLayout/LeftNav.tsx
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/components/MainLayout/LeftNav.tsx
@@ -1,5 +1,5 @@
 import React, { memo, MouseEvent, FC } from 'react';
-import { Segment, Image, Flex } from '@fluentui/react-northstar';
+import { Segment, Image, Flex, Tooltip } from '@fluentui/react-northstar';
 import { NavLink } from 'react-router-dom';
 import { useProject } from '../../hooks/useProject';
 import { useCameras } from '../../hooks/useCameras';
@@ -12,38 +12,66 @@ const LeftNav: FC<any> = ({ styles, disabled }): JSX.Element => {
   return (
     <Segment color="grey" inverted styles={{ ...styles, padding: 0, paddingTop: '1em' }}>
       <Flex column gap="gap.large" hAlign="center">
-        <NavItem disabled={disabled} src="/icons/location.png" to="/locations"></NavItem>
+        <NavItem title="Location" disabled={disabled} src="/icons/location.png" to="/locations"></NavItem>
         <NavItem
+          title="Camera"
           disabled={disabled}
           src="/icons/camera.png"
           to={cameraInUsed ? `/cameras/detail?name=${cameraInUsed.name}` : '/cameras'}
         />
-        <NavItem disabled={disabled} src="/icons/part.png" to="/parts"></NavItem>
-        <NavItem disabled={disabled} src="/icons/partIdentification.png" to="/partIdentification"></NavItem>
-        <NavItem disabled={disabled} src="/icons/manual.png" to="/manual"></NavItem>
-        <NavItem disabled={disabled} src="/icons/pretrained-model.png" to="/pretrainDetection"></NavItem>
+        <NavItem title="Part" disabled={disabled} src="/icons/part.png" to="/parts"></NavItem>
+        <NavItem
+          title="Part Identification"
+          disabled={disabled}
+          src="/icons/partIdentification.png"
+          to="/partIdentification"
+        ></NavItem>
+        <NavItem
+          title="Manual Identification"
+          disabled={disabled}
+          src="/icons/manual.png"
+          to="/manual"
+        ></NavItem>
+        <NavItem
+          title="Demo Model"
+          disabled={disabled}
+          src="/icons/pretrained-model.png"
+          to="/pretrainDetection"
+        ></NavItem>
       </Flex>
     </Segment>
   );
 };
 
 interface NavItemProps {
+  title: string;
   src: string;
   to: string;
   disabled: boolean;
 }
-const NavItem: FC<NavItemProps> = ({ src, to, disabled }): JSX.Element => {
+const NavItem: FC<NavItemProps> = ({ title, src, to, disabled }): JSX.Element => {
   return (
-    <NavLink
-      to={to}
-      style={{ display: 'flex', justifyContent: 'center', padding: '0.8em', cursor: disabled && 'default' }}
-      activeStyle={{ backgroundColor: 'rgba(250, 83, 5, 0.5)' }}
-      onClick={(e: MouseEvent): void => {
-        if (disabled) e.preventDefault();
-      }}
-    >
-      <Image src={src} design={{ width: '100%' }} />
-    </NavLink>
+    <Tooltip
+      content={title}
+      position="after"
+      trigger={
+        <NavLink
+          to={to}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            padding: '0.8em',
+            cursor: disabled && 'default',
+          }}
+          activeStyle={{ backgroundColor: 'rgba(250, 83, 5, 0.5)' }}
+          onClick={(e: MouseEvent): void => {
+            if (disabled) e.preventDefault();
+          }}
+        >
+          <Image src={src} design={{ width: '100%' }} />
+        </NavLink>
+      }
+    />
   );
 };
 
