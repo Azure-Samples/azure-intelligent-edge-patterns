0f66dbab51189ee046d0d2971ba595caaeb9aa1e
Andrew Kao <andrewkao@linkernetworks.com>
2020-07-20 14:49:35 +0800

    Add Button to LeftNav

--- factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/components/MainLayout/LeftNav.tsx
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/components/MainLayout/LeftNav.tsx
@@ -1,8 +1,16 @@
 import React, { memo, MouseEvent, FC } from 'react';
-import { Segment, Image, Flex } from '@fluentui/react-northstar';
+import { Segment, Image, Flex, Box, mergeStyles } from '@fluentui/react-northstar';
 import { NavLink } from 'react-router-dom';
 import { useProject } from '../../hooks/useProject';
 import { useCameras } from '../../hooks/useCameras';
+import FeedbackDialog from '../FeedbackDialog';
+
+const itemStyles = (disabled): React.CSSProperties => ({
+  display: 'flex',
+  justifyContent: 'center',
+  padding: '0.8em',
+  cursor: disabled && 'default',
+});
 
 const LeftNav: FC<any> = ({ styles, disabled }): JSX.Element => {
   const project = useProject(false);
@@ -21,6 +29,20 @@ const LeftNav: FC<any> = ({ styles, disabled }): JSX.Element => {
         <NavItem disabled={disabled} src="/icons/part.png" to="/parts"></NavItem>
         <NavItem disabled={disabled} src="/icons/partIdentification.png" to="/partIdentification"></NavItem>
         <NavItem disabled={disabled} src="/icons/manual.png" to="/manual"></NavItem>
+        <FeedbackDialog
+          trigger={
+            <Box
+              styles={mergeStyles(itemStyles(disabled), {
+                ':hover': { cursor: 'pointer' },
+              })()}
+              onClick={(e: MouseEvent): void => {
+                if (disabled) e.preventDefault();
+              }}
+            >
+              <Image src="/icons/feedback.png" design={{ width: '100%' }} />
+            </Box>
+          }
+        />
       </Flex>
     </Segment>
   );
@@ -35,7 +57,7 @@ const NavItem: FC<NavItemProps> = ({ src, to, disabled }): JSX.Element => {
   return (
     <NavLink
       to={to}
-      style={{ display: 'flex', justifyContent: 'center', padding: '0.8em', cursor: disabled && 'default' }}
+      style={itemStyles(disabled)}
       activeStyle={{ backgroundColor: 'rgba(250, 83, 5, 0.5)' }}
       onClick={(e: MouseEvent): void => {
         if (disabled) e.preventDefault();
