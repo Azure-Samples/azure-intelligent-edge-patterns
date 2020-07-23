e6a0d9d7aa65a95aefb4123322008bd96c9d81a8
goatwu1993 <peteeelol@gmail.com>
2020-07-23 17:58:50 +0800

    Remove script. Remove app_insight middleware in backend

--- factory-ai-vision/EdgeSolution/modules/WebModule/backend/configs/app_insight.py
+++ factory-ai-vision/EdgeSolution/modules/WebModule/backend/configs/app_insight.py
@@ -3,6 +3,7 @@
 If APP_INSIGHT_ON, django will run middleware that send
 backend message to Application Insight
 """
-APP_INSIGHT_ON = True
+
+APP_INSIGHT_ON = False
 APP_INSIGHT_INST_KEY = 'c192ed15-f8a9-4adb-8c17-44e0f8c3d3cd'
 APP_INSIGHT_CONN_STR = f'InstrumentationKey={APP_INSIGHT_INST_KEY}'
