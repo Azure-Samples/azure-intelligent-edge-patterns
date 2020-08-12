3fddacc422a88966ef1673d10900689cddce6c0a
goatwu1993 <peteeelol@gmail.com>
2020-08-12 13:15:03 +0800

    Init

--- factory-ai-vision/EdgeSolution/modules/WebModule/backend/configs/api_router.py
+++ factory-ai-vision/EdgeSolution/modules/WebModule/backend/configs/api_router.py
@@ -4,6 +4,9 @@
 from django.conf.urls import url
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from vision_on_edge.azure_app_insight.api import views as app_insight_views
 from vision_on_edge.azure_parts.api import views as azure_part_views
@@ -25,6 +28,7 @@ from vision_on_edge.streams.api import views as stream_views
 
 router = DefaultRouter()
 router.trailing_slash = '/?'
+app_name = "api"
 
 router.register('settings', azure_setting_views.SettingViewSet)
 router.register('cameras', views.CameraViewSet)
@@ -42,6 +46,31 @@ router.register('notifications', notifications_views.NotificationViewSet)
 router.register('images', image_views.ImageViewSet)
 
 urlpatterns = router.urls
+
+SchemaView = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        SchemaView.without_ui(cache_timeout=0),
+        name='schema-json'),
+    url(r'^swagger/$',
+        SchemaView.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'),
+    url(r'^redoc/$',
+        SchemaView.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'),
+]
 
 urlpatterns += [
     url('streams/connect', stream_views.connect_stream),
@@ -70,8 +99,4 @@ urlpatterns += [
     path('relabel/update', relabel_views.relabel_update),
     path('appinsight/key', app_insight_views.instrumentation_key),
     path('camera_utils/verify_rtsp', camera_util_views.verify_rtsp),
-    # FIXME PETER please find a better place to put this
-    path('notifications/delete_all', notifications_views.delete_all)
 ]
-
-app_name = "api"
