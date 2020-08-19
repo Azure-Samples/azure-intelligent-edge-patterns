6fd073096a090a51a816652c4f7d3c352b9f2aba
Andrew Kao <andrewkao@linkernetworks.com>
2020-08-19 16:25:49 +0800

    Remove redundant file

--- factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/store/store.ts
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/store/store.ts
@@ -1,10 +1,7 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: {},
 });
 
 export type RootState = ReturnType<typeof store.getState>;
