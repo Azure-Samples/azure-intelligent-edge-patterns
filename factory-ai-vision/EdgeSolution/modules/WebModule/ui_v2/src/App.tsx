e29b4c60fdea0c4bfc594d64e84450149de33975
Andrew Kao <andrewkao@linkernetworks.com>
2020-08-20 14:33:03 +0800

    Add MainLayout
    Including LeftNav & Top Nav

--- factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/App.tsx
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { MainLayout } from './components/MainLayout';
 
 function App() {
-  return <MainLayout />;
+  return (
+    <MainLayout>
+      <h1>Hello World</h1>
+    </MainLayout>
+  );
 }
 
 export default App;
--- factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/components/MainLayout.tsx
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/components/MainLayout.tsx
@@ -1,53 +1,16 @@
 import React from 'react';
-import { Nav, INavLinkGroup, INavStyles } from '@fluentui/react';
+import { TopNav } from './TopNav';
+import { LeftNav } from './LeftNav';
+import { Stack } from '@fluentui/react';
 
-const navLinksGroup: INavLinkGroup[] = [
-  {
-    links: [
-      {
-        name: 'Home',
-        url: '/',
-        iconProps: {
-          imageProps: {
-            src: '/icon/home.png',
-          },
-        },
-      },
-      {
-        name: 'Cameras',
-        url: '/cameras',
-        iconProps: {
-          imageProps: {
-            src: '/icon/cameras.png',
-          },
-        },
-      },
-      {
-        name: 'Images',
-        url: '/images',
-        iconProps: {
-          imageProps: {
-            src: '/icon/images.png',
-          },
-        },
-      },
-      {
-        name: 'Parts',
-        url: '/parts',
-        iconProps: {
-          imageProps: {
-            src: '/icon/parts.png',
-          },
-        },
-      },
-    ],
-  },
-];
-
-const navStyles: Partial<INavStyles> = {
-  root: { width: '206px', borderRight: '1px solid #eee', height: '100vh' },
-};
-
-export const MainLayout: React.FC = () => {
-  return <Nav groups={navLinksGroup} styles={navStyles} />;
+export const MainLayout: React.FC = ({ children }) => {
+  return (
+    <Stack styles={{ root: { height: '100vh' } }}>
+      <TopNav />
+      <Stack grow={1} horizontal>
+        <LeftNav />
+        <div>{children}</div>
+      </Stack>
+    </Stack>
+  );
 };
