f622ca38e412b668eab6c9fe02d2fd3c621acc1a
Andrew Kao <andrewkao@linkernetworks.com>
2020-08-20 15:07:48 +0800

    Add Router

--- factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/App.tsx
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/App.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import { MainLayout } from './components/MainLayout';
+import { RootRouter } from './routes/RootRouter';
+import TelemetryProvider from './components/TelemetryProvider';
+import { useWebSocket } from './hooks/useWebSocket';
+import { useDispatch, useSelector } from 'react-redux';
+import { State } from 'RootStateType';
+import { thunkGetSettingAndAppInsightKey } from './store/setting/settingAction';
 
 function App() {
+  // Listen for the notification boardcast.
+  useWebSocket();
+
+  const dispatch = useDispatch();
+  const appInsightKey = useSelector<State, string>((state) => state.setting.appInsightKey);
+  const isAppInsightOn = useSelector<State, boolean>((state) => state.setting.isCollectData);
+
+  useEffect(() => {
+    dispatch(thunkGetSettingAndAppInsightKey());
+  }, [dispatch]);
+
   return (
-    <MainLayout>
-      <h1>Hello World</h1>
-    </MainLayout>
+    <BrowserRouter>
+      <TelemetryProvider instrumentationKey={appInsightKey} isAppInsightOn={isAppInsightOn}>
+        <MainLayout>
+          <RootRouter />
+        </MainLayout>
+      </TelemetryProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Home: React.FC = () => {
+  return <h1>Home</h1>;
+};
