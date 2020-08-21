4fc6d8b62d5b2f2b14f8e98db5222fcdcf47422b
Andrew Kao <andrewkao@linkernetworks.com>
2020-08-21 11:07:00 +0800

    Add Camera Panel

--- factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/pages/Cameras.tsx
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/pages/Cameras.tsx
@@ -1,5 +1,63 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import {
+  CommandBar,
+  ICommandBarItemProps,
+  getTheme,
+  Panel,
+  TextField,
+  PrimaryButton,
+  DefaultButton,
+  Stack,
+} from '@fluentui/react';
+import { useConstCallback } from '@uifabric/react-hooks';
+
+const theme = getTheme();
 
 export const Cameras: React.FC = () => {
-  return <h1>Cameras</h1>;
+  const [isPanelOpen, setPanelOpen] = useState(false);
+
+  const dismissPanel = useConstCallback(() => setPanelOpen(false));
+
+  const commandBarItems: ICommandBarItemProps[] = useMemo(
+    () => [
+      {
+        key: 'addBtn',
+        text: 'Add',
+        iconProps: {
+          iconName: 'Add',
+        },
+        onClick: () => setPanelOpen(true),
+      },
+    ],
+    [],
+  );
+
+  const onRenderFooterContent = useConstCallback(() => (
+    <Stack tokens={{ childrenGap: 5 }} horizontal>
+      <PrimaryButton onClick={dismissPanel}>Add</PrimaryButton>
+      <DefaultButton onClick={dismissPanel}>Cancel</DefaultButton>
+    </Stack>
+  ));
+
+  return (
+    <>
+      <CommandBar
+        items={commandBarItems}
+        styles={{ root: { borderBottom: `solid 1px ${theme.palette.neutralLight}` } }}
+      />
+      <Panel
+        isOpen={isPanelOpen}
+        onDismiss={dismissPanel}
+        hasCloseButton
+        headerText="Add Camera"
+        isHiddenOnDismiss
+        onRenderFooterContent={onRenderFooterContent}
+        isFooterAtBottom={true}
+      >
+        <TextField label="Camera name" required />
+        <TextField label="RTSP URL" required />
+        <TextField label="Location" required />
+      </Panel>
+    </>
+  );
 };
