2eb067548bc2cfdce59cce1671d30dd995c16031
Andrew Kao <andrewkao@linkernetworks.com>
2020-08-24 11:17:30 +0800

    Add Camera Details

---
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/components/RTSPVideo/RTSPVideo.tsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import Axios from 'axios';
+
+import { RTSPVideoProps, CaptureLabelMode } from './RTSPVideo.type';
+import { useInterval } from '../../hooks/useInterval';
+import { Stack, ActionButton } from '@fluentui/react';
+
+// TODO Check if we need two mode for capturing & the capturing UX
+export const RTSPVideoComponent: React.FC<RTSPVideoProps> = ({
+  rtsp = null,
+  partId = null,
+  canCapture,
+  onCapturePhoto,
+  autoPlay,
+}) => {
+  const [streamId, setStreamId] = useState<string>('');
+  const [captureLabelMode, setCaptureLabelMode] = useState<CaptureLabelMode>(CaptureLabelMode.PerImage);
+
+  const onCreateStream = useCallback((): void => {
+    const url =
+      partId === null
+        ? `/api/streams/connect/?rtsp=${rtsp}`
+        : `/api/streams/connect/?part_id=${partId}&rtsp=${rtsp}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data?.status === 'ok') {
+          setStreamId(data.stream_id);
+        }
+        return null;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [partId, rtsp]);
+
+  const onDisconnect = (): void => {
+    setStreamId('');
+    fetch(`/api/streams/${streamId}/disconnect`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        return null;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  useInterval(
+    () => {
+      Axios.get(`/api/streams/${streamId}/keep_alive`).catch(console.error);
+    },
+    streamId ? 3000 : null,
+  );
+
+  useEffect(() => {
+    window.addEventListener('beforeunload', onDisconnect);
+    return (): void => {
+      window.removeEventListener('beforeunload', onDisconnect);
+    };
+  });
+
+  useEffect(() => {
+    if (autoPlay) onCreateStream();
+  }, [autoPlay, onCreateStream]);
+
+  const src = streamId ? `/api/streams/${streamId}/video_feed` : '';
+
+  return (
+    <Stack>
+      <ActionButton iconProps={{ iconName: 'Camera' }}>Capture image</ActionButton>
+      <div style={{ width: '100%', height: '100%', backgroundColor: 'black' }}>
+        {src ? <img src={src} style={{ width: '100%', height: '100%', objectFit: 'contain' }} /> : null}
+      </div>
+    </Stack>
+  );
+};
+
+export const RTSPVideo = React.memo(RTSPVideoComponent);
--- factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/pages/CameraDetails.tsx
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/pages/CameraDetails.tsx
@@ -1,5 +1,115 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  Breadcrumb,
+  Stack,
+  CommandBar,
+  ICommandBarItemProps,
+  getTheme,
+  IBreadcrumbItem,
+  Text,
+  IStackTokens,
+  ITextStyles,
+  Spinner,
+  MessageBar,
+  MessageBarButton,
+} from '@fluentui/react';
+import { useQuery } from '../hooks/useQuery';
+import { State } from 'RootStateType';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectCameraById, getCameras } from '../store/cameraSlice';
+import { RTSPVideo } from '../components/RTSPVideo';
+import { thunkGetProject } from '../store/project/projectActions';
+
+const theme = getTheme();
+const titleStyles: ITextStyles = { root: { fontWeight: 600, fontSize: '16px' } };
+const infoBlockTokens: IStackTokens = { childrenGap: 10 };
 
 export const CameraDetails: React.FC = () => {
-  return <h1>CameraDetails</h1>;
+  const camerId = parseInt(useQuery().get('cameraId'), 10);
+  const camera = useSelector((state: State) => selectCameraById(state, camerId));
+  const projectCameraId = useSelector((state: State) => state.project.data.camera);
+  const dispatch = useDispatch();
+
+  const commandBarItems: ICommandBarItemProps[] = [
+    {
+      key: 'edit',
+      text: 'Edit',
+      iconProps: {
+        iconName: 'Edit',
+      },
+    },
+    {
+      key: 'delete',
+      text: 'Delete',
+      iconProps: {
+        iconName: 'Delete',
+      },
+    },
+  ];
+
+  useEffect(() => {
+    dispatch(getCameras(false));
+    dispatch(thunkGetProject(false));
+  }, [dispatch]);
+
+  if (camera === undefined) return <Spinner label="Loading" />;
+
+  const breadCrumbItems: IBreadcrumbItem[] = [
+    { key: 'cameras', text: 'Cameras', href: '/cameras' },
+    { key: camera.name, text: camera.name },
+  ];
+
+  const isCameraInUsed = projectCameraId === camerId;
+
+  return (
+    <Stack styles={{ root: { height: '100%' } }}>
+      <CommandBar
+        items={commandBarItems}
+        styles={{ root: { borderBottom: `solid 1px ${theme.palette.neutralLight}` } }}
+      />
+      <CameraInUsedMsgBar isInUsed={isCameraInUsed} />
+      <Stack tokens={{ childrenGap: 30 }} styles={{ root: { padding: '15px' } }} grow>
+        <Breadcrumb items={breadCrumbItems} />
+        <Stack tokens={{ childrenGap: 20 }} horizontal grow>
+          <CameraInfo rtsp={camera.rtsp} location="" />
+          <Stack style={{ width: '80%' }}>
+            <Text styles={titleStyles}>Live feed</Text>
+            <RTSPVideo rtsp={camera.rtsp} canCapture autoPlay onCapturePhoto={() => {}} />
+          </Stack>
+        </Stack>
+      </Stack>
+    </Stack>
+  );
 };
+
+const CameraInUsedMsgBar: React.FC<{ isInUsed: boolean }> = ({ isInUsed }) => {
+  const [visible, setVisible] = useState(true);
+
+  if (isInUsed && visible)
+    return (
+      <MessageBar
+        actions={<MessageBarButton>View Task</MessageBarButton>}
+        onDismiss={() => setVisible(false)}
+        isMultiline={false}
+      >
+        This camera is currently in use.
+      </MessageBar>
+    );
+
+  return null;
+};
+
+const CameraInfo: React.FC<{ rtsp: string; location: string }> = ({ rtsp, location }) => (
+  <Stack tokens={{ childrenGap: 30 }} styles={{ root: { width: '20%', marginLeft: '0.8em' } }}>
+    <Stack tokens={infoBlockTokens}>
+      <Text styles={titleStyles}>RTSP URL</Text>
+      <Text block nowrap>
+        {rtsp}
+      </Text>
+    </Stack>
+    <Stack tokens={infoBlockTokens}>
+      <Text styles={titleStyles}>Location</Text>
+      <Text>{/**TODO add location */}</Text>
+    </Stack>
+  </Stack>
+);
