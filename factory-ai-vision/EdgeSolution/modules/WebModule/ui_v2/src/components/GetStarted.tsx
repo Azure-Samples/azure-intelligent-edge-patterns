22aa71870fe661bf773db74e1274b582977994bd
Andrew Kao <andrewkao@linkernetworks.com>
2020-08-25 11:38:49 +0800

    Add home page basic layout

---
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/components/GetStarted.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const GetStarted: React.FC = () => {
+  return <h1>Get started</h1>;
+};
--- factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/pages/Home.tsx
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/pages/Home.tsx
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { Stack, CommandBar, getTheme, ICommandBarItemProps, Pivot, PivotItem } from '@fluentui/react';
+import { GetStarted } from '../components/GetStarted';
+
+const theme = getTheme();
 
 export const Home: React.FC = () => {
-  return <h1>Home</h1>;
+  const commandBarItems: ICommandBarItemProps[] = useMemo(
+    () => [
+      {
+        key: 'addBtn',
+        text: 'New Task',
+        iconProps: {
+          iconName: 'Add',
+        },
+        onClick: () => {},
+      },
+    ],
+    [],
+  );
+
+  return (
+    <Stack styles={{ root: { height: '100%' } }}>
+      <CommandBar
+        items={commandBarItems}
+        styles={{ root: { borderBottom: `solid 1px ${theme.palette.neutralLight}` } }}
+      />
+      <Stack styles={{ root: { padding: '15px' } }} grow>
+        <Pivot>
+          <PivotItem headerText="Get started">
+            <GetStarted />
+          </PivotItem>
+          <PivotItem headerText="Task">Task</PivotItem>
+        </Pivot>
+      </Stack>
+    </Stack>
+  );
 };
