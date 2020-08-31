248dce21ae98003d3223349a3534566617e9995f
Andrew Kao <andrewkao@linkernetworks.com>
2020-08-31 10:51:02 +0800

    Add capture dialog in image page

--- factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/pages/Images.tsx
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/pages/Images.tsx
@@ -1,11 +1,17 @@
-import * as React from 'react';
+import React, { useState, useMemo } from 'react';
 import { ICommandBarItemProps, Stack, CommandBar, getTheme, Breadcrumb } from '@fluentui/react';
 import { EmptyAddIcon } from '../components/EmptyAddIcon';
+import { CaptureDialog } from '../components/CaptureDialog';
+import { CaptureLabelMode } from '../components/RTSPVideo/RTSPVideo.type';
 
 const theme = getTheme();
 
 export const Images: React.FC = () => {
-  const commandBarItems: ICommandBarItemProps[] = React.useMemo(
+  const [isCaptureDialgOpen, setCaptureDialogOpen] = useState(false);
+  const openCaptureDialog = () => setCaptureDialogOpen(true);
+  const closeCaptureDialog = () => setCaptureDialogOpen(false);
+
+  const commandBarItems: ICommandBarItemProps[] = useMemo(
     () => [
       {
         key: 'uploadImages',
@@ -21,27 +27,34 @@ export const Images: React.FC = () => {
         iconProps: {
           iconName: 'Camera',
         },
-        onClick: () => {},
+        onClick: openCaptureDialog,
       },
     ],
     [],
   );
 
   return (
-    <Stack styles={{ root: { height: '100%' } }}>
-      <CommandBar
-        items={commandBarItems}
-        styles={{ root: { borderBottom: `solid 1px ${theme.palette.neutralLight}` } }}
-      />
-      <Stack styles={{ root: { padding: '15px' } }} grow>
-        <Breadcrumb items={[{ key: 'images', text: 'Images' }]} />
-        <EmptyAddIcon
-          title="Add images"
-          subTitle="Capture images from your video streams and tag parts"
-          primary={{ text: 'Capture from camera', onClick: () => {} }}
-          secondary={{ text: 'Upload images', onClick: () => {} }}
+    <>
+      <Stack styles={{ root: { height: '100%' } }}>
+        <CommandBar
+          items={commandBarItems}
+          styles={{ root: { borderBottom: `solid 1px ${theme.palette.neutralLight}` } }}
         />
+        <Stack styles={{ root: { padding: '15px' } }} grow>
+          <Breadcrumb items={[{ key: 'images', text: 'Images' }]} />
+          <EmptyAddIcon
+            title="Add images"
+            subTitle="Capture images from your video streams and tag parts"
+            primary={{ text: 'Capture from camera', onClick: openCaptureDialog }}
+            secondary={{ text: 'Upload images', onClick: () => {} }}
+          />
+        </Stack>
       </Stack>
-    </Stack>
+      <CaptureDialog
+        captureLabelMode={CaptureLabelMode.PerImage}
+        isOpen={isCaptureDialgOpen}
+        onDismiss={closeCaptureDialog}
+      />
+    </>
   );
 };
