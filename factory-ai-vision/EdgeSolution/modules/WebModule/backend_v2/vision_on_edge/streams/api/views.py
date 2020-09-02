00f02c4f6b17ad19a9333be125a82724b2e627d6
goatwu1993 <peteeelol@gmail.com>
2020-09-02 15:11:31 +0800

    Stream get camera id

--- factory-ai-vision/EdgeSolution/modules/WebModule/backend_v2/vision_on_edge/streams/api/views.py
+++ factory-ai-vision/EdgeSolution/modules/WebModule/backend_v2/vision_on_edge/streams/api/views.py
@@ -11,22 +11,28 @@ import time
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.files.images import ImageFile
-from django.http import HttpResponse, JsonResponse, StreamingHttpResponse
+from django.http import HttpResponse, StreamingHttpResponse
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
+from ...general.api.serializers import SimpleErrorSerializer
 from ...azure_iot.utils import inference_module_url
 from ...azure_parts.models import Part
+from ...cameras.models import Camera
 from ...images.api.serializers import ImageSerializer
 from ...images.models import Image
 from ..models import Stream
+from .serializers import ConnectStreamResponseSerializer, CaptureStreamResponseSerializer
 
 logger = logging.getLogger(__name__)
 
 # Stream Views
 #
-STREAM_GC_TIME_THRESHOLD = 5  # Seconds
-PRINT_STREAMS = False
+STREAM_GC_TIME_THRESHOLD = 5000  # Seconds
+PRINT_STREAMS = True
 
 
 class StreamManager():
@@ -97,23 +103,58 @@ if 'runserver' in sys.argv:
     stream_manager = StreamManager()
 
 
-@api_view()
+@swagger_auto_schema(
+    method='get',
+    operation_summary='Get training performace from Custom Vision.',
+    manual_parameters=[
+        openapi.Parameter('rtsp',
+                          openapi.IN_QUERY,
+                          type=openapi.TYPE_STRING,
+                          description='RTSP'),
+        openapi.Parameter('part_id',
+                          openapi.IN_QUERY,
+                          type=openapi.TYPE_STRING,
+                          description='Part ID',
+                          required=False)
+    ],
+    responses={
+        '200': ConnectStreamResponseSerializer,
+        '400': SimpleErrorSerializer
+    })
+@api_view(['GET'])
 def connect_stream(request):
-    """Connect to stream"""
-    part_id = request.query_params.get("part_id")
-    rtsp = request.query_params.get("rtsp") or "0"
-    if part_id is None:
-        s = Stream(rtsp, part_id=None)
-        stream_manager.add(s)
-        return Response({"status": "ok", "stream_id": s.id})
+    """connect_stream.
 
-    try:
-        Part.objects.get(pk=int(part_id))
-        s = Stream(rtsp, part_id=int(part_id))
-        stream_manager.add(s)
-        return Response({"status": "ok", "stream_id": s.id})
-    except ObjectDoesNotExist:
-        return Response({"status": "failed", "reason": "part_id doesnt exist"})
+    Args:
+        request:
+    """
+    part_id = request.query_params.get("part_id") or None
+    rtsp = request.query_params.get("rtsp") or "0"
+    if part_id is not None:
+        try:
+            Part.objects.get(pk=part_id)
+        except ObjectDoesNotExist:
+            return Response(
+                {
+                    "status": "failed",
+                    "log": "part_id doesnt exist."  # yapf
+                },
+                status=status.HTTP_400_BAD_REQUEST)
+        part_id = (int(part_id))
+    if not Camera.objects.filter(rtsp=rtsp).exists():
+        return Response(
+            {
+                "status": "failed",
+                "log": "rtsp given does not belong to any camera."
+            },
+            status=status.HTTP_400_BAD_REQUEST)
+    camera_id = Camera.objects.filter(rtsp=rtsp).first().id
+    stream_obj = Stream(rtsp=rtsp, camera_id=camera_id, part_id=part_id)
+    stream_manager.add(stream_obj)
+    response_data = {"status": "ok", "stream_id": stream_obj.id}
+    serializer = ConnectStreamResponseSerializer(data=response_data)
+    if serializer.is_valid(raise_exception=True):
+        return Response(serializer.validated_data)
 
 
 @api_view()
@@ -124,12 +165,14 @@ def disconnect_stream(request, stream_id):
     stream = stream_manager.get_stream_by_id(stream_id)
     if stream:
         stream.close()
-        return JsonResponse({'status': 'ok'})
+        return Response({'status': 'ok'})
 
-    return Response({
-        "status": "failed",
-        "reason": "cannot find stream_id " + str(stream_id)
-    })
+    return Response(
+        {
+            "status": "failed",
+            "reason": "cannot find stream_id " + str(stream_id)
+        },
+        status=status.HTTP_400_BAD_REQUEST)
 
 
 def video_feed(request, stream_id):
@@ -142,14 +185,32 @@ def video_feed(request, stream_id):
             stream.gen(),
             content_type="multipart/x-mixed-replace;boundary=frame")
 
-    return HttpResponse("<h1>Unknown Stream " + str(stream_id) + " </h1>")
+    return HttpResponse("<h1>Unknown Stream " + str(stream_id) + " </h1>",
+                        status=status.HTTP_400_BAD_REQUEST)
 
 
+@swagger_auto_schema(
+    method='get',
+    operation_summary='Get training performace from Custom Vision.',
+    manual_parameters=[
+        openapi.Parameter('stream_id',
+                          openapi.IN_PATH,
+                          type=openapi.TYPE_INTEGER,
+                          description='Stream ID'),
+        openapi.Parameter('stream_id',
+                          openapi.IN_QUERY,
+                          type=openapi.TYPE_INTEGER,
+                          description='Part ID',
+                          required=False),
+    ],
+    responses={
+        '200': CaptureStreamResponseSerializer,
+        '400': SimpleErrorSerializer
+    })
 @api_view()
 def capture(request, stream_id):
     """Capture image
     """
-
     stream = stream_manager.get_stream_by_id(stream_id)
     if stream:
         img_data = stream.get_frame()
@@ -159,22 +220,22 @@ def capture(request, stream_id):
         logger.info(stream)
         logger.info(stream.part_id)
         part_id = request.query_params.get("part_id") or stream.part_id
-        if not part_id:
-            return JsonResponse({
-                "status": "failed",
-                "reason": "neither Stream and capture request have part_id"
-            })
-        img_obj = Image(image=img, part_id=part_id)
+        camera_id = stream.camera_id
+        img_obj = Image(image=img, part_id=part_id, camera_id=camera_id)
         img_obj.save()
-        img_serialized = ImageSerializer(img_obj, context={"request": request})
-        logger.info(img_serialized.data)
+        img_serializer = ImageSerializer(img_obj, context={"request": request})
+        response_data = {"status": "ok", "image": img_serializer.data}
+        #response_serializer = CaptureStreamResponseSerializer(
+        #    data=response_data)
+        #if response_serializer.is_valid(raise_exception=True):
+        return Response(response_data)
 
-        return Response({"status": "ok", "image": img_serialized.data})
-
-    return Response({
-        "status": "failed",
-        "reason": "cannot find stream_id " + str(stream_id)
-    })
+    return Response(
+        {
+            "status": "failed",
+            "log": "cannot find stream_id " + str(stream_id)
+        },
+        status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view()
@@ -186,12 +247,14 @@ def keep_alive(request, stream_id):
     stream = stream_manager.get_stream_by_id(stream_id)
     if stream:
         stream.update_keep_alive()
-        return JsonResponse({'status': 'ok'})
+        return Response({'status': 'ok'})
 
-    return JsonResponse({
-        "status": "failed",
-        "reason": "cannot find stream_id " + str(stream_id)
-    })
+    return Response(
+        {
+            "status": "failed",
+            "reason": "cannot find stream_id " + str(stream_id)
+        },
+        status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view()
