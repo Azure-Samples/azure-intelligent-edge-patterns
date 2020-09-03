51c8b97a535f37182885767c4d92918d5215e461
goatwu1993 <peteeelol@gmail.com>
2020-09-03 18:14:50 +0800

    Fix exception handler error

--- factory-ai-vision/EdgeSolution/modules/WebModule/backend_v2/vision_on_edge/general/api/exception_handlers.py
+++ factory-ai-vision/EdgeSolution/modules/WebModule/backend_v2/vision_on_edge/general/api/exception_handlers.py
@@ -4,7 +4,7 @@
 
 from rest_framework.response import Response
 from rest_framework.views import exception_handler
-from rest_framework.exceptions import APIException
+from rest_framework.exceptions import APIException, ValidationError
 from .serializers import MSStyleErrorResponseSerializer
 
 
@@ -18,11 +18,13 @@ def ms_style_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
     if isinstance(exc, APIException) and response is not None:
-        res_data = {}
-        response.data['status_code'] = response.status_code
-        response.data['code'] = exc.detail.code
-        response.data['message'] = str(exc.detail)
-        res_data["error"] = response.data
+        res_data = {"error": {}}
+        res_data["error"]['status_code'] = response.status_code
+        if isinstance(exc, ValidationError):
+            res_data["error"]['code'] = 'validation_error'
+        else:
+            res_data["error"]['code'] = exc.detail.code
+        res_data["error"]['message'] = str(exc.detail)
         serializer = MSStyleErrorResponseSerializer(data=res_data)
         if serializer.is_valid(raise_exception=False):
             return Response(serializer.validated_data,
