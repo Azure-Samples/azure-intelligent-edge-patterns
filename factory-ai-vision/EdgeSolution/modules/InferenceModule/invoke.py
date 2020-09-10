8f112dfb9a2409dfccf491eb08fdf37f56cd7f3d
Willy Kuo <waitingkuo0527@gmail.com>
2020-09-10 12:17:47 +0800

    update invoke

--- factory-ai-vision/EdgeSolution/modules/InferenceModule/invoke.py
+++ factory-ai-vision/EdgeSolution/modules/InferenceModule/invoke.py
@@ -8,16 +8,18 @@ import urllib.request
 from azure.iot.hub import IoTHubRegistryManager
 from azure.iot.hub.models import CloudToDeviceMethod, CloudToDeviceMethodResult
 
+from config import IOTHUB_CONNECTION_STRING, DEVICE_ID
+
+MODULE_ID = 'lvaEdge'
 
 default_payload = {"@apiVersion": "1.0"}
 
 
 class GraphManager:
     def __init__(self):
-        self.conn = 'HostName=customvision.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=1If/+cNQswsLkTKejbjWQZMpBt2q96x/z3vapuB3Ew0='
-        self.registry_manager = IoTHubRegistryManager(self.conn)
-        self.device_id = 'testcam'
-        self.module_id = 'lvaEdge'
+        self.registry_manager = IoTHubRegistryManager(IOTHUB_CONNECTION_STRING)
+        self.device_id = DEVICE_ID
+        self.module_id = MODULE_ID
 
     def invoke_method(self, method_name, payload):
         module_method = CloudToDeviceMethod(
@@ -25,3 +27,37 @@ class GraphManager:
         res = self.registry_manager.invoke_device_module_method(
             self.device_id, self.module_id, module_method)
         return res.as_dict()
+
+    def invoke_graph_topology_get(self, name):
+        method = 'GraphTopologyGet'
+        payload = {
+            "@apiVersion": "1.0",
+            "name": name,
+        }
+        return self.invoke_method(method, payload)
+
+    def invoke_graph_topology_set(self, name, properties):
+        method = 'GraphTopologySet'
+        payload = {
+            "@apiVersion": "1.0",
+            'name': name,
+            'properties': properties,
+        }
+        return self.invoke_method(method, payload)
+
+    def invoke_graph_topology_list(self):
+        method = 'GraphTopologyList'
+        payload = {
+            '@apiVersion': '1.0',
+        }
+        return self.invoke_method(method, payload)
+
+    def invoke_graph_topology_delete(self, name):
+        method = 'GraphTopologyList'
+        payload = {
+            '@apiVersion': '1.0',
+            'name': name,
+        }
+        return self.invoke_method(method, payload)
+
+
