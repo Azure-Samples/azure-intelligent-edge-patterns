bc95095697fce7da53b4a1d373c8d4f973bbd030
Andrew Kao <andrewkao@linkernetworks.com>
2020-09-21 15:31:03 +0800

    Add notification slice

--- factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/store/rootReducer.ts
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/store/rootReducer.ts
@@ -4,7 +4,7 @@ import partsReducer from './partSlice';
 import locationsReducer from './locationSlice';
 import projectReducer from './project/projectReducer';
 import settingReducer from './setting/settingReducer';
-// import notificationReducer from '../reducers/notificationReducer';
+import notificationReducer from './notificationSlice';
 import imagesReducer from './imageSlice';
 import annotationReducer from './annotationSlice';
 import labelingPageReducer from './labelingPageSlice';
@@ -17,11 +17,9 @@ import scenarioReducer from './scenarioSlice';
 export const rootReducer = combineReducers({
   project: projectReducer,
   setting: settingReducer,
-  // The Below state has been refactor
   camera: cameraReducer,
   locations: locationsReducer,
-  // TODO
-  // notifications: notificationReducer,
+  notifications: notificationReducer,
   parts: partsReducer,
   labelImages: imagesReducer,
   annotations: annotationReducer,
