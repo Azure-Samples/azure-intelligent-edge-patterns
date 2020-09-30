ed0d9733f79d2f14d8d718194f4915ab0e92c25a
Andrew Kao <andrewkao@linkernetworks.com>
2020-09-30 15:06:16 +0800

    Fix vertical seperator issue

--- factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/components/Deployment.tsx
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui_v2/src/components/Deployment.tsx
@@ -220,7 +220,8 @@ export const Deployment: React.FC = () => {
             />
           </Stack>
         </Stack>
-        <Separator vertical />
+        {/* Vertical seperator has z-index in 1 as default, which will be on top of the panel */}
+        <Separator vertical styles={{ root: { zIndex: 0 } }} />
         <Stack styles={{ root: { width: '435px' } }}>
           <Pivot styles={{ root: { borderBottom: `solid 1px ${palette.neutralLight}` } }}>
             <PivotItem headerText="Insights">
