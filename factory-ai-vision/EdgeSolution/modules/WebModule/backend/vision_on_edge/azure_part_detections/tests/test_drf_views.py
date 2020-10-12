c361648393416cd3270477cc20dc75fdafbfe4af
goatwu1993 <peteeelol@gmail.com>
2020-10-12 12:45:03 +0800

    Add test mocking frontend api call

--- factory-ai-vision/EdgeSolution/modules/WebModule/backend/vision_on_edge/azure_part_detections/tests/test_drf_views.py
+++ factory-ai-vision/EdgeSolution/modules/WebModule/backend/vision_on_edge/azure_part_detections/tests/test_drf_views.py
@@ -28,3 +28,30 @@ def test_get(part_detection):
     response_body = response.content.decode("utf-8")
 
     assert PartDetectionSerializer(part_detection).data in json.loads(response_body)
+
+
+def test_put_from_frontend(part_detection):
+    data = {
+        "parts": [part.id for part in part_detection.parts.all()],
+        "cameras": [cam.id for cam in part_detection.cameras.all()],
+        "project": part_detection.project.id,
+        "needRetraining": True,
+        "accuracyRangeMin": 60,
+        "accuracyRangeMax": 80,
+        "maxImages": 20,
+        "metrics_is_send_iothub": False,
+        "metrics_frame_per_minutes": 6,
+        "name": "New Task ",
+        "send_video_to_cloud": False,
+        "inference_mode": "PD",
+        "fps": 10,
+        "inference_protocol": "grpc",
+    }
+    factory = APIRequestFactory()
+    pd_list_view = PartDetectionViewSet.as_view({"put": "update"})
+    request = factory.put("/fake-url/", data=data)
+
+    response = pd_list_view(request, pk=part_detection.id).render()
+    response_body = response.content.decode("utf-8")
+    print(response_body)
+    assert response.status_code == 200
