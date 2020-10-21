29e5803508fbb52cffc9b2b8f47b5dc6268f2ff1
goatwu1993 <peteeelol@gmail.com>
2020-10-21 12:39:17 +0800

    Add recommend fps field in inference module

--- factory-ai-vision/EdgeSolution/modules/WebModule/backend/vision_on_edge/inference_modules/models.py
+++ factory-ai-vision/EdgeSolution/modules/WebModule/backend/vision_on_edge/inference_modules/models.py
@@ -3,6 +3,7 @@
 
 import logging
 
+import requests
 from django.db import models
 
 logger = logging.getLogger(__name__)
@@ -14,6 +15,17 @@ class InferenceModule(models.Model):
     name = models.CharField(max_length=200)
     url = models.CharField(max_length=1000, unique=True)
     is_gpu = models.BooleanField(default=False)
+
+    def recommended_fps(self) -> int:
+        try:
+            response = requests.get("http://" + self.url + "/recommended_fps")
+            result = int(response.json()["fps"])
+        except Exception:
+            logger.exception(
+                "Get recommended_fps from inference module failed. Fallback to default"
+            )
+            result = 10
+        return result
 
     def __str__(self):
         return self.name
