4f7d7c0d8b30402a943b4a061a60701fb4b4a655
goatwu1993 <peteeelol@gmail.com>
2020-12-15 20:19:27 +0800

    Remove apt-get to try arm

--- factory-ai-vision/Deploy/arm/scripts/deploy-iot-edge.sh
+++ factory-ai-vision/Deploy/arm/scripts/deploy-iot-edge.sh
@@ -31,20 +31,34 @@ echo "$(info) Installing apt-packages "
 # apt-get update && apt-get install -y jq coreutils unzip
 echo "$(info) Apt-packages installed"
 
-printf "\n%60s\n" " " | tr ' ' '-'
-echo "$(info) Installing pip packages."
-python -m pip -q install --upgrade pip
-echo "$(info) Installing iotedgedev."
-pip -q install --upgrade iotedgedev==${IOTEDGE_DEV_VERSION}
+# printf "\n%60s\n" " " | tr ' ' '-'
+# echo "$(info) Installing pip packages."
+# python -m pip -q install --upgrade pip
+# echo "$(info) Installing iotedgedev."
+# pip -q install --upgrade iotedgedev==${IOTEDGE_DEV_VERSION}
 
-echo "$(info) Updating Azure CLI."
-pip -q install --upgrade azure-cli
-pip -q install --upgrade azure-cli-telemetry
+# echo "$(info) Updating Azure CLI."
+# pip -q install --upgrade azure-cli
+# pip -q install --upgrade azure-cli-telemetry
 
-printf "\n%60s\n" " " | tr ' ' '-'
-echo "$(info) Installing Azure IoT extension."
+# printf "\n%60s\n" " " | tr ' ' '-'
+# echo "$(info) Installing Azure IoT extension."
+# az extension add --name azure-iot
+# pip -q install --upgrade jsonschema
+echo "Installing iotedgedev"
+pip install iotedgedev==2.1.4
+
+echo "Updating az-cli"
+pip install --upgrade azure-cli
+pip install --upgrade azure-cli-telemetry
+
+echo "installing azure iot extension"
 az extension add --name azure-iot
-pip -q install --upgrade jsonschema
+
+pip3 install --upgrade jsonschema
+apk add coreutils
+echo "Installation complete"
+
 
 printf "\n%60s\n" " " | tr ' ' '-'
 echo "$(info) Checking IoT Hub and IoT Edge Device."
