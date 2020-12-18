9bf4f0124d85b60bc0400c4c2a1ccb592b04b9e3
goatwu1993 <peteeelol@gmail.com>
2020-12-18 12:39:38 +0800

    Update helper scripts

--- factory-ai-vision/DevTools/update_version.py
+++ factory-ai-vision/DevTools/update_version.py
@@ -1,12 +1,19 @@
+"""Set version helper
+"""
 #!/usr/bin/env python
+
 import argparse
+
+import semantic_version
 
 from utils_file import FileContext
 from utils_iotedge import get_modules
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('type', choices=['patch', 'minor', 'major'], help="Update type")
+    parser.add_argument('type', choices=['patch', 'minor', 'major', 'set'], help="Update type")
+    parser.add_argument('--version', type=semantic_version.Version, help="set version")
     args = parser.parse_args()
 
     fc = FileContext(__file__)
@@ -19,3 +26,7 @@ if __name__ == "__main__":
             module.next_minor()
         elif args.type == 'major':
             module.next_major()
+        elif args.type == 'set':
+            if not args.version:
+                parser.error('please provide version number (--version)')
+            module.version = args.version
