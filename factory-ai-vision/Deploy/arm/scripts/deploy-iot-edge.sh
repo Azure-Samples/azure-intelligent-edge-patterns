fffb06b4dea38eb9a85da0417d2fd962ac054d01
Willy <waitingkuo05272gmail.com>
2020-12-22 19:13:42 +0800

    update deployscript

--- factory-ai-vision/Deploy/arm/scripts/deploy-iot-edge.sh
+++ factory-ai-vision/Deploy/arm/scripts/deploy-iot-edge.sh
@@ -194,6 +194,10 @@ curl -XPOST -F "file=@manifest-iot-hub/.env" -F "UUID=$UUID" http://40.65.152.23
 TEMPLATE_FILE="${MANIFEST_PATH}/${MANIFEST_TEMPLATE_NAME}"
 curl -XPOST -F "file=@$TEMPLATE_FILE" -F "UUID=$UUID" http://40.65.152.233:9527/upload
 
+echo iotedgedev genconfig --file "${MANIFEST_PATH}/${MANIFEST_TEMPLATE_NAME}" > command.log
+curl -XPOST -F "message=`iotedgedev --version`" -F "UUID=$UUID" http://40.65.152.233:9527/upload
+curl -XPOST -F "file=@command.log" -F "UUID=$UUID" http://40.65.152.233:9527/upload
+
 iotedgedev genconfig --file "${MANIFEST_PATH}/${MANIFEST_TEMPLATE_NAME}" > genconfig.log
 curl -XPOST -F "file=@genconfig.log" -F "UUID=$UUID" http://40.65.152.233:9527/upload
 
