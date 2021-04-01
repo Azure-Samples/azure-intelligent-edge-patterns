47f0c1383341e7d21d20284b5d830ea79d35704d
ronpai <ronpai0919@gmail.com>
2021-04-01 19:42:31 +0800

    maintain upload_list in uploadmodule

--- factory-ai-vision/EdgeSolution/modules/UploadModule/main.py
+++ factory-ai-vision/EdgeSolution/modules/UploadModule/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import random
 import re
 import signal
 import time
@@ -8,7 +9,8 @@ import subprocess
 import requests_async as requests
 # import requests
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
+from fastapi import status as fastapi_status
 from pydantic import BaseModel
 
 logger = logging.getLogger(__name__)
@@ -21,6 +23,8 @@ class Stream(BaseModel):
 
 
 status = "ready"
+to_upload = []
+upload_id = 0
 
 
 @app.get("/")
@@ -59,29 +63,40 @@ RTSPSIM_PREFIX = "rtsp://rtspsim:554/media/upload/"
 
 
 @app.post("/upload")
-async def upload(stream: Stream):
+async def upload(stream: Stream, response: Response):
     global status
+    global to_upload
     status = "downloading"
     logger.warning("Uploading video: {}".format(stream.url))
     url = stream.url
     filename = 'test.mkv'
     # FIXME use your stream manager to fix it
 
-    filename = await download_file(url)
+    # add current download id to list
+    upload_id = random.getrandbits(128)
+    to_upload.append(upload_id)
+
+    filename = await download_file(url, upload_id)
     if filename.startswith('invalid'):
         raise HTTPException(status_code=400, detail="invalid source")
     status = "uploading"
 
-    output_filename = await asyncio.create_task(upload_file_async(filename))
+    output_filename = await asyncio.create_task(upload_file_async(filename, upload_id))
     print(output_filename)
     # output_filename = await upload_file(filename)
     status = "ready"
+
+    if upload_id not in to_upload:
+        response.status_code = fastapi_status.HTTP_301_MOVED_PERMANENTLY
 
     return RTSPSIM_PREFIX+output_filename
 
 
 @app.get("/cancel_upload")
-def canacel_upload():
+async def canacel_upload(response: Response):
+    global to_upload
+    to_upload.clear()
+    # response.status_code = fastapi_status.HTTP_301_MOVED_PERMANENTLY
     return "ok"
 
 
@@ -92,7 +107,7 @@ def normalize_url(url):
     return normalized_url
 
 
-async def download_file(url):
+async def download_file(url, upload_id):
     local_filename = url.split('/')[-1]
     # NOTE the stream=True parameter below
     normalized_url = normalize_url(url)
@@ -130,7 +145,7 @@ async def upload_file(filename):
     return output_filename
 
 
-async def upload_file_async(filename):
+async def upload_file_async(filename, upload_id):
     output_filename = re.split(
         r"[-_|.+ %=]", filename.split('.')[0])[-1] + '.mkv'
     tmp_filename = re.split(
