4d4dc8add057014630bc94b4ed340b23849e8961
ronpai <ronpai0919@gmail.com>
2021-07-22 12:14:36 +0800

    fix(webmodule): multipule customvision set default when app starts

--- factory-ai-vision/EdgeSolution/modules/WebModule/backend/vision_on_edge/azure_projects/helpers.py
+++ factory-ai-vision/EdgeSolution/modules/WebModule/backend/vision_on_edge/azure_projects/helpers.py
@@ -14,13 +14,16 @@ def create_default_objects():
     """create_default_objects"""
     logger.info("Create/update a default project.")
     setting_obj = Setting.objects.first()
-    Project.objects.update_or_create(
-        is_demo=False,
-        setting=setting_obj,
-        defaults={
-            "prediction_uri": prediction_module_url(),
-        },
-    )
+    project_obj = Project.objects.filter(is_demo=False, setting=setting_obj).first()
+    setattr(project_obj, "prediction_uri", prediction_module_url())
+    project_obj.save()
+    # Project.objects.update_or_create(
+    #     is_demo=False,
+    #     setting=setting_obj,
+    #     defaults={
+    #         "prediction_uri": prediction_module_url(),
+    #     },
+    # )
 
 
 def create_demo_objects():
