372577b0cd92924ec8cbe1ecce0a8bfc737bded3
ronpai <ronpai0919@gmail.com>
2021-09-17 12:47:04 +0800

    fix(webmodule): capitalize

--- factory-ai-vision/EdgeSolution/modules/WebModule/backend/vision_on_edge/azure_training_status/progress.py
+++ factory-ai-vision/EdgeSolution/modules/WebModule/backend/vision_on_edge/azure_training_status/progress.py
@@ -2,45 +2,45 @@
 """
 
 PROGRESS_1_FINDING_PROJECT: dict = {
-    "status": "finding project",
+    "status": "Finding project",
     "log": "Finding Project on Custom Vision.",
 }
 PROGRESS_2_PROJECT_FOUND: dict = {
-    "status": "uploading project",
+    "status": "Uploading project",
     "log": "Project found on Custom Vision.",
 }
 PROGRESS_2_PROJECT_CREATED: dict = {
-    "status": "uploading project",
+    "status": "Uploading project",
     "log": "Creating project on Custom Vision.",
 }
 
 PROGRESS_3_UPLOADING_PARTS: dict = {
-    "status": "uploading parts",
+    "status": "Uploading parts",
     "log": "Uploading parts to Custom Vision.",
 }
 PROGRESS_4_UPLOADING_IMAGES: dict = {
-    "status": "uploading images",
+    "status": "Uploading images",
     "log": "Uploading images to Custom Vision.",
 }
 PROGRESS_5_SUBMITTING_TRAINING_TASK: dict = {
-    "status": "preparing training task",
+    "status": "Preparing training task",
     "log": "Submitting training task to Custom Vision.",
 }
 PROGRESS_6_PREPARING_CUSTOM_VISION_ENV: dict = {
-    "status": "preparing custom vision environment",
+    "status": "Preparing custom vision environment",
     "log": "Preparing Custom Vision Environment.",
 }
 PROGRESS_7_TRAINING: dict = {
-    "status": "training",
+    "status": "Training",
     "log": "Training on Custom Vision (may take 10-15 minutes).",
 }
 PROGRESS_8_EXPORTING: dict = {
-    "status": "exporting",  # Change line
+    "status": "Exporting",  # Change line
     "log": "Exporting model.",
 }
 # Complete
 PROGRESS_0_OK: dict = {"status": "ok", "log": "Model trained."}
-PROGRESS_9_SUCCESS: dict = {"status": "success", "log": "Model trained."}
+PROGRESS_9_SUCCESS: dict = {"status": "Success", "log": "Model trained."}
 PROGRESS_10_NO_CHANGE: dict = {"status": "No change", "log": "Project not changed. Not Training!"}
 
 # Failed
