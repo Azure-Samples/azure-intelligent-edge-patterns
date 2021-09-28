577e12ed8a68c848e76e353df99a5bf3e868ce9c
froggy <qscgyujm@gmail.com>
2021-09-28 12:16:09 +0800

    [FIX] fix model error

--- factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/components/Models/ModelContainer.tsx
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/components/Models/ModelContainer.tsx
@@ -17,7 +17,7 @@ import { selectAllIntelProject } from '../../store/IntelProjectSlice';
 
 import Model from './Model';
 import AddCustomVisionContainer from './Panel/AddCustomVisionContainer';
-import IntelProjectDashboard from './IntelProjectDashboard';
+import OpenVinoDashboard from './OpenVinoDashboard';
 
 const getClasses = () =>
   mergeStyleSets({
@@ -158,7 +158,7 @@ const ModelContainer = () => {
         <Stack tokens={{ childrenGap: '65px' }}>
           <CommandBar styles={{ root: { marginTop: '24px' } }} items={newCommandBarItems} />
           {isAddIntelOvms ? (
-            <IntelProjectDashboard
+            <OpenVinoDashboard
               intelProjectList={intelProjectList}
               openVinoProjectList={trainingProjectList.filter((project) => project.category === 'openvino')}
               onCloseIntel={() => setIsAddIntelOvms(false)}
