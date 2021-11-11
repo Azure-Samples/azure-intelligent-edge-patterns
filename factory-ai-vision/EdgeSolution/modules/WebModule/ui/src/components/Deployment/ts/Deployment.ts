e546ea4760adf3178b768e13f16f3e797680c3c3
froggy <qscgyujm@gmail.com>
2021-11-11 16:11:54 +0800

    [CHANGE] change deployment ui

--- factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/components/Deployment/ts/Deployment.ts
+++ factory-ai-vision/EdgeSolution/modules/WebModule/ui/src/components/Deployment/ts/Deployment.ts
@@ -4,3 +4,20 @@ export type AdditionalProps = {
 };
 
 export type DeploymentProps = AdditionalProps;
+
+type ScenarioMetricsName = 'violation' | 'all_objects';
+
+type ScenarioMetrics = {
+  name: ScenarioMetricsName;
+  count: { [key: string]: { current: number; total: number } } | null;
+};
+
+export type InferenceMetrics = {
+  average_time: number;
+  count: { [key: string]: number } | null;
+  device: string;
+  inference_num: { [key: string]: number } | number;
+  scenario_metrics: ScenarioMetrics[];
+  success_rate: { [key: string]: number } | null;
+  unidentified_num: { [key: string]: number } | null;
+};
