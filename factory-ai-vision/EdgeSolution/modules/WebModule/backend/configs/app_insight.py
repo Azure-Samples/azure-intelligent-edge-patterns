e705a5de696e9e630dd25f4d00767a4077ee8d3d
ronpai <ronpai0919@gmail.com>
2022-04-14 18:53:15 +0800

    build: add application insights key to installer

--- factory-ai-vision/EdgeSolution/modules/WebModule/backend/configs/app_insight.py
+++ factory-ai-vision/EdgeSolution/modules/WebModule/backend/configs/app_insight.py
@@ -3,6 +3,8 @@
 If APP_INSIGHT_ON, django will run middleware that send
 backend message to Application Insight
 """
+import os
 
-APP_INSIGHT_INST_KEY = "4c8b219e-5c92-4934-bd2c-62003fb061c4"
+APP_INSIGHT_INST_KEY = ""
+APP_INSIGHT_INST_KEY = os.environ.get('APPLICATIONINSIGHTS_INSTRUMENTATION_KEY', APP_INSIGHT_INST_KEY)
 APP_INSIGHT_CONN_STR = f"InstrumentationKey={APP_INSIGHT_INST_KEY}"
